c8cce2fc5107aad79a025ca71a091024c545a672
Milada Milli <milada.majak@gmail.com>
2019-12-02 10:54:33 +0100

    Remove unused test

--- test/advent_of_code2019_test.exs
+++ test/advent_of_code2019_test.exs
@@ -2,7 +2,7 @@ defmodule AdventOfCode2019Test do
   use ExUnit.Case
   doctest AdventOfCode2019
 
-  test "greets the world" do
-    assert AdventOfCode2019.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert AdventOfCode2019.hello() == :world
+  # end
 end
