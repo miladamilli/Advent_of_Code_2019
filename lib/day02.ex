4fc08b6230e1d65da3201d3e9fa97566a0c1bb20
Milada Milli <milada.majak@gmail.com>
2019-12-04 17:35:27 +0100

    Day02 Refactor

--- lib/day02.ex
+++ lib/day02.ex
@@ -1,36 +1,25 @@
 defmodule Day02 do
-  import AdventOfCode2019
-
   @input "day02"
   @value1 12
   @value2 2
-  def part_one() do
-    [first, _, _ | rest] =
-      @input
-      |> read_file()
-      |> read_numbers()
 
-    data = [first | [@value1 | [@value2 | rest]]]
-
-    [position0, _] = String.split(decode(data), ",", parts: 2)
-    IO.inspect(position0)
+  # part one
+  def intcode_program() do
+    [first, _, _ | rest] = @input |> ReadInput.file() |> ReadInput.numbers()
+    data = [first, @value1, @value2 | rest]
+    [position0, _] = String.split(decode(data, data), ",", parts: 2)
+    position0
   end
 
-  def decode(data) do
-    first(data, data)
+  def decode([], all) do
+    List.flatten(all) |> Enum.join(",")
   end
 
-  def first([], all) do
-    List.flatten(all)
-    |> Enum.join(",")
+  def decode([99 | _rest], all) do
+    List.flatten(all) |> Enum.join(",")
   end
 
-  def first([99 | _rest], all) do
-    List.flatten(all)
-    |> Enum.join(",")
-  end
-
-  def first([operation, first, second, value | rest], all) do
+  def decode([operation, first, second, value | rest], all) do
     first = Enum.at(all, first)
     second = Enum.at(all, second)
     {part1, part2} = Enum.split(all, value)
@@ -43,32 +32,25 @@ defmodule Day02 do
 
     all = List.flatten([part1] ++ [new_value] ++ tl(part2))
 
-    first(Enum.take(all, -length(rest)), all)
+    decode(Enum.take(all, -length(rest)), all)
   end
 
-  def part_two() do
-    data = read_file(@input)
-    data = read_numbers(data)
-
-    [first, _, _ | rest] = data
+  # part two
+  @expected "19690720"
+  def find_input_pair() do
+    [first, _, _ | rest] = ReadInput.file(@input) |> ReadInput.numbers()
 
     input_pairs =
       for noun <- 0..99, verb <- 0..99 do
         {noun, verb}
       end
 
-    result =
+    [{noun, verb}] =
       Enum.filter(input_pairs, fn {noun, verb} ->
-        data = [first | [noun | [verb | rest]]]
-
-        check(first(data, data))
+        data = [first, noun, verb | rest]
+        match?(@expected <> _, decode(data, data))
       end)
 
-    [{noun, verb}] = result
-    IO.inspect(100 * noun + verb)
+    100 * noun + verb
   end
-
-  @expected "19690720"
-  defp check(@expected <> _), do: true
-  defp check(_), do: false
 end
--- test/day02_test.exs
+++ test/day02_test.exs
@@ -3,19 +3,24 @@ defmodule Day02Test do
   doctest Day02
 
   test "intcode program" do
-    assert Day02.decode(AdventOfCode2019.read_numbers("1,0,0,0,99")) == "2,0,0,0,99"
-    assert Day02.decode(AdventOfCode2019.read_numbers("2,3,0,3,99")) == "2,3,0,6,99"
-    assert Day02.decode(AdventOfCode2019.read_numbers("2,4,4,5,99,0")) == "2,4,4,5,99,9801"
+    input1 = ReadInput.numbers("1,0,0,0,99")
+    assert Day02.decode(input1, input1) == "2,0,0,0,99"
 
-    assert Day02.decode(AdventOfCode2019.read_numbers("1,1,1,4,99,5,6,0,99")) ==
-             "30,1,1,4,2,5,6,0,99"
+    input2 = ReadInput.numbers("2,3,0,3,99")
+    assert Day02.decode(input2, input2) == "2,3,0,6,99"
+
+    input3 = ReadInput.numbers("2,4,4,5,99,0")
+    assert Day02.decode(input3, input3) == "2,4,4,5,99,9801"
+
+    input4 = ReadInput.numbers("1,1,1,4,99,5,6,0,99")
+    assert Day02.decode(input4, input4) == "30,1,1,4,2,5,6,0,99"
   end
 
-  test "puzzle answer part 1" do
-    assert Day02.part_one() == "3654868"
+  test "part 1 answer" do
+    assert Day02.intcode_program() == "3654868"
   end
 
-  test "puzzle answer part 2" do
-    assert Day02.part_two() == 7014
+  test "part 2 answer" do
+    assert Day02.find_input_pair() == 7014
   end
 end
