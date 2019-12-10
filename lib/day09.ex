cdfb3d5076d5f635bd991aa5f5687b6693c78efe
Milada Milli <milada.majak@gmail.com>
2019-12-10 09:17:51 +0100

    Day 09 Big refactor

--- lib/day09.ex
+++ lib/day09.ex
@@ -1,9 +1,16 @@
 defmodule Day09 do
   @input_file "day09"
 
-  def run() do
+  def run1() do
     program = @input_file |> ReadInput.file() |> load_program()
     computer = init_computer(program, 1)
+
+    run_program(computer).output
+  end
+
+  def run2() do
+    program = @input_file |> ReadInput.file() |> load_program()
+    computer = init_computer(program, 2)
 
     run_program(computer).output
   end
@@ -16,124 +23,123 @@ defmodule Day09 do
   end
 
   def init_computer(program, input) do
-    %{memory: program, position: 0, input: [input], output: nil, halt: false, relative_base: 0}
+    %{memory: program, pc: 0, input: [input], output: nil, halt: false, relative_base: 0}
   end
 
   def run_program(%{halt: true} = computer), do: computer
 
   def run_program(computer) do
-    code = computer.memory[computer.position]
-    code = Enum.take([0, 0, 0] ++ Integer.digits(code), -4)
+    code = computer.memory[computer.pc]
+    code = Enum.take([0, 0, 0, 0] ++ Integer.digits(code), -5)
+
     computer = run_instr(code, computer)
     run_program(computer)
   end
 
-  defp run_instr([_, _, 9, 9], computer), do: %{computer | halt: true}
-  defp run_instr([mode2, mode1, _, 1], computer), do: op(:add, mode1, mode2, computer)
-  defp run_instr([mode2, mode1, _, 2], computer), do: op(:multiply, mode1, mode2, computer)
-  defp run_instr([_, _, _, 3], computer), do: op(:input, computer)
-  defp run_instr([_, mode1, _, 4], computer), do: op(:output, mode1, computer)
-  defp run_instr([mode2, mode1, _, 5], computer), do: op(:nonzero, mode1, mode2, computer)
-  defp run_instr([mode2, mode1, _, 6], computer), do: op(:zero, mode1, mode2, computer)
-  defp run_instr([mode2, mode1, _, 7], computer), do: op(:less, mode1, mode2, computer)
-  defp run_instr([mode2, mode1, _, 8], computer), do: op(:equal, mode1, mode2, computer)
-  defp run_instr([_, mode1, _, 9], computer), do: op(:offset, mode1, computer)
+  defp run_instr([_, _, _, 9, 9], computer), do: %{computer | halt: true}
 
-  defp op(:input, %{memory: memory, position: position} = computer) do
+  defp run_instr([mode3, mode2, mode1, _, 1], computer),
+    do: op(:add, mode1, mode2, mode3, computer)
+
+  defp run_instr([mode3, mode2, mode1, _, 2], computer),
+    do: op(:multiply, mode1, mode2, mode3, computer)
+
+  defp run_instr([_, _, mode, _, 3], computer), do: op(:input, mode, computer)
+  defp run_instr([_, _, mode, _, 4], computer), do: op(:output, mode, computer)
+  defp run_instr([_, mode2, mode1, _, 5], computer), do: op(:jump_if_true, mode1, mode2, computer)
+
+  defp run_instr([_, mode2, mode1, _, 6], computer),
+    do: op(:jump_if_false, mode1, mode2, computer)
+
+  defp run_instr([mode3, mode2, mode1, _, 7], computer),
+    do: op(:less_than, mode1, mode2, mode3, computer)
+
+  defp run_instr([mode3, mode2, mode1, _, 8], computer),
+    do: op(:equals, mode1, mode2, mode3, computer)
+
+  defp run_instr([_, _, mode1, _, 9], computer), do: op(:offset, mode1, computer)
+
+  defp op(:input, mode, %{memory: memory, pc: pc} = computer) do
     if computer.input == [] do
       computer
     else
-      memory = update_memory(memory, memory[position + 1], hd(computer.input))
+      memory = update_memory(memory, read_for_write(mode, pc + 1, computer), hd(computer.input))
       computer = %{computer | input: tl(computer.input)}
-      update_computer(computer, memory, position + 2)
+      update_computer(computer, memory, pc + 2)
     end
   end
 
-  defp op(:add, mode1, mode2, %{memory: memory, position: position} = computer) do
-    value1 = read(mode1, position + 1, computer)
-    value2 = read(mode2, position + 2, computer)
-    #  IO.inspect  value1 + value2
-    memory = update_memory(memory, memory[position + 3], value1 + value2)
-    update_computer(computer, memory, position + 4)
+  defp op(:output, mode, %{memory: memory, pc: pc} = computer) do
+    computer = %{computer | output: read(mode, pc + 1, computer)}
+    update_computer(computer, memory, pc + 2)
   end
 
-  defp op(:multiply, mode1, mode2, %{memory: memory, position: position} = computer) do
-    value1 = read(mode1, position + 1, computer)
-    value2 = read(mode2, position + 2, computer)
-
-    # if program[position + 3] == 1000 do
-    #   IO.inspect  value2
-    # end
-    # IO.inspect  value1
-
-    memory = update_memory(memory, memory[position + 3], value1 * value2)
-    update_computer(computer, memory, position + 4)
+  defp op(:offset, mode1, %{pc: pc, relative_base: base} = computer) do
+    %{computer | relative_base: base + read(mode1, pc + 1, computer), pc: pc + 2}
   end
 
-  defp op(:nonzero, mode1, mode2, %{memory: memory, position: position} = computer) do
-    if read(mode1, position + 1, computer) != 0 do
-      update_computer(
-        computer,
-        memory,
-        read(mode2, position + 2, computer)
-      )
+  defp op(:add, mode1, mode2, mode3, %{memory: memory, pc: pc} = computer) do
+    value1 = read(mode1, pc + 1, computer)
+    value2 = read(mode2, pc + 2, computer)
+    value3 = read_for_write(mode3, pc + 3, computer)
+    memory = update_memory(memory, value3, value1 + value2)
+    update_computer(computer, memory, pc + 4)
+  end
+
+  defp op(:multiply, mode1, mode2, mode3, %{memory: memory, pc: pc} = computer) do
+    value1 = read(mode1, pc + 1, computer)
+    value2 = read(mode2, pc + 2, computer)
+    value3 = read_for_write(mode3, pc + 3, computer)
+    memory = update_memory(memory, value3, value1 * value2)
+    update_computer(computer, memory, pc + 4)
+  end
+
+  defp op(:less_than, mode1, mode2, mode3, %{memory: memory, pc: pc} = computer) do
+    value3 = read_for_write(mode3, pc + 3, computer)
+
+    if read(mode1, pc + 1, computer) < read(mode2, pc + 2, computer) do
+      memory = update_memory(memory, value3, 1)
+      update_computer(computer, memory, pc + 4)
     else
-      update_computer(computer, memory, position + 3)
+      memory = update_memory(memory, value3, 0)
+      update_computer(computer, memory, pc + 4)
     end
   end
 
-  defp op(:zero, mode1, mode2, %{memory: memory, position: position} = computer) do
-    if read(mode1, position + 1, computer) == 0 do
-      update_computer(
-        computer,
-        memory,
-        read(mode2, position + 2, computer)
-      )
+  defp op(:equals, mode1, mode2, mode3, %{memory: memory, pc: pc} = computer) do
+    value3 = read_for_write(mode3, pc + 3, computer)
+
+    if read(mode1, pc + 1, computer) == read(mode2, pc + 2, computer) do
+      memory = update_memory(memory, value3, 1)
+      update_computer(computer, memory, pc + 4)
     else
-      update_computer(computer, memory, position + 3)
+      memory = update_memory(memory, value3, 0)
+      update_computer(computer, memory, pc + 4)
     end
   end
 
-  defp op(:less, mode1, mode2, %{memory: memory, position: position} = computer) do
-    if read(mode1, position + 1, computer) < read(mode2, position + 2, computer) do
-      memory = update_memory(memory, memory[position + 3], 1)
-      update_computer(computer, memory, position + 4)
+  defp op(:jump_if_true, mode1, mode2, %{memory: memory, pc: pc} = computer) do
+    if read(mode1, pc + 1, computer) != 0 do
+      update_computer(computer, memory, read(mode2, pc + 2, computer))
     else
-      memory = update_memory(memory, memory[position + 3], 0)
-      update_computer(computer, memory, position + 4)
+      update_computer(computer, memory, pc + 3)
     end
   end
 
-  defp op(:equal, mode1, mode2, %{memory: memory, position: position} = computer) do
-    if read(mode1, position + 1, computer) == read(mode2, position + 2, computer) do
-      memory = update_memory(memory, memory[position + 3], 1)
-      update_computer(computer, memory, position + 4)
+  defp op(:jump_if_false, mode1, mode2, %{memory: memory, pc: pc} = computer) do
+    if read(mode1, pc + 1, computer) == 0 do
+      update_computer(computer, memory, read(mode2, pc + 2, computer))
     else
-      memory = update_memory(memory, memory[position + 3], 0)
-      update_computer(computer, memory, position + 4)
+      update_computer(computer, memory, pc + 3)
     end
   end
 
-  defp op(:output, mode1, %{memory: memory, position: position} = computer) do
-    output = read(mode1, position + 1, computer)
-    computer = %{computer | output: output}
-    update_computer(computer, memory, position + 2)
+  defp update_computer(computer, memory, pc) do
+    %{computer | memory: memory, pc: pc}
   end
 
-  defp op(:offset, mode1, %{position: position, relative_base: base} = computer) do
-    %{
-      computer
-      | relative_base: base + read(mode1, position + 1, computer),
-        position: position + 2
-    }
-  end
-
-  defp update_computer(computer, memory, position) do
-    %{computer | memory: memory, position: position}
-  end
-
-  defp update_memory(memory, position, value) do
-    Map.put(memory, position, value)
+  defp update_memory(memory, pc, value) do
+    Map.put(memory, pc, value)
   end
 
   defp read(0, address, %{memory: memory}),
@@ -143,6 +149,12 @@ defmodule Day09 do
 
   defp read(2, address, %{memory: memory, relative_base: relative_base}),
     do: read_memory(memory[read_memory(memory[address]) + relative_base])
+
+  defp read_for_write(0, address, %{memory: memory}),
+    do: read_memory(memory[address])
+
+  defp read_for_write(2, address, %{memory: memory, relative_base: relative_base}),
+    do: read_memory(memory[address]) + relative_base
 
   defp read_memory(nil), do: 0
   defp read_memory(value), do: value
--- test/day09_test.exs
+++ test/day09_test.exs
@@ -1,18 +1,24 @@
 defmodule Day09Test do
   use ExUnit.Case
 
-  test "output 1" do
-    program = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
-    computer = Day09.init_computer(Day09.load_program(program), 1)
-    computer = Day09.run_program(computer)
-   # IO.inspect computer
-  #  assert Enum.join(Map.values(computer.program), ",") == program
+  test "output 2" do
+    program = Day09.load_program("1102,34915192,34915192,7,4,7,99,0")
+    computer = Day09.init_computer(program, 1)
+    output = Day09.run_program(computer).output
+    assert length(Integer.digits(output)) == 16
   end
 
-  # test "output 2" do
-  #   program = Day09.load_program("1102,34915192,34915192,7,4,7,99,0")
-  #   computer = Day09.init_computer(program, 1)
-  #   output = Day09.run_program(computer).output
-  #   assert length(Integer.digits(output)) == 16
-  # end
+  test "output 3" do
+    program = Day09.load_program("104,1125899906842624,99")
+    computer = Day09.init_computer(program, 1)
+    assert Day09.run_program(computer).output == 1_125_899_906_842_624
+  end
+
+  test "part 1 answer" do
+    assert Day09.run1() == 4_234_906_522
+  end
+
+  test "part 2 answer" do
+    assert Day09.run2() == 60962
+  end
 end
